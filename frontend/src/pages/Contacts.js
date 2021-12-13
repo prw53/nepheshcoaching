17c4a8822414fe5fc51e6b35a29d428f6d855ce8
Peter <peter.waller53@gmail.com>
2021-12-13 15:21:18 +0000

    Email being sent OK using emailjs

--- frontend/src/pages/Contacts.js
+++ frontend/src/pages/Contacts.js
@@ -2,7 +2,6 @@ import { Box, Button, Card, CardContent, Grid, TextField, Typography } from '@ma
 import React, { useState, useRef } from 'react';
 import emailjs from 'emailjs-com';
 
-// import axios from 'axios';
 
 const initialFValues = {
   firstName: "",
@@ -13,13 +12,7 @@ const initialFValues = {
 
 function Contacts() {
   const [sent, setSent] = useState(false);
-  const [text, setText] = useState("");
   const [values, setValues] = useState(initialFValues);
-
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
 
   const handleInputChange = e => {
     const { name, value } = e.target
@@ -33,60 +26,21 @@ function Contacts() {
 
   const sendEmail = (e) => {
     e.preventDefault();
-    // alert(values.firstName, values.lastName, values.email, values.message)
-    // alert(firstName + lastName + email + message)
-    // console.log(values)
-
-    alert(values.firstName + values.lastName + values.email + values.message)
-    setSent(true)
-
-    // emailjs.sendForm('service_f6ynrg8', 'template_i63ce1y', form.current, 'user_AiIgMvHTL545jVQ7Y2tRb')
-    //   .then((result) => {
-    //     setSent(true)
-    //     console.log(result.text);
-    //   }, (error) => {
-    //     console.log(error.text);
-    //   });
+    emailjs.sendForm('service_f6ynrg8', 'template_i63ce1y', form.current, 'user_AiIgMvHTL545jVQ7Y2tRb')
+      .then((result) => {
+        setSent(true)
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
   };
-
-  // const handleSendEmail = async () => {
-  //   setSent(true)
-  //   setText(
-  //     'First Name: ' + initialFValues.firstName +
-  //     'Last Name: ' + initialFValues.lastName
-  //   )
-  //   try {
-  //     console.log('Trying catch')
-  //     // await axios.post("http://localhost:4000/send_mail", {
-  //     //   text: text,
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // };
 
   return (
     <Box marginTop={3} marginBottom={3}>
-      {/* <Typography variant='h3'>This is the Contacts page!</Typography> */}
       <Card style={{ maxWidth: 450, margin: '0 auto', padding: '5px 5px' }}>
         <CardContent>
           {!sent ? (
             <form ref={form} onSubmit={sendEmail}>
-
-             
-                {/* <label>Name</label>
-                <input type="text" name="firstName" />
-                <label>Name</label>
-                <input type="text" name="lastName" />
-                <label>Email</label>
-                <input type="email" name="email" />
-                <label>Message</label>
-                <textarea name="message" />
-                <input type="submit" value="Send" /> */}
-           
-
-              {/* <input type='text' value={text} onChange={(e) => setText(e.target.value)} />
-              <button type='submit' >Send Email</button> */}
-
               <Typography gutterBottom variant='h5' align='center'>Contact Form</Typography>
               <Typography gutterBottom color='textSecondary' variant='body2' align='center' component='p'>Submit your details and I will get back to you shortly</Typography>
               <Grid container spacing={1}>
@@ -94,8 +48,6 @@ function Contacts() {
                   <TextField
                     label='First Name'
                     name='firstName'
-                    // value={firstName} 
-                    // onChange={(e) => setFirstName(e.target.value)}
                     value={values.firstName}
                     onChange={handleInputChange}
                     placeholder='Enter first name'
@@ -107,8 +59,6 @@ function Contacts() {
                   <TextField
                     label='Last Name'
                     name='lastName'
-                    // value={lastName} 
-                    // onChange={(e) => setLastName(e.target.value)}
                     value={values.lastName}
                     onChange={handleInputChange}
                     placeholder='Enter last name'
@@ -121,8 +71,6 @@ function Contacts() {
                     type='email'
                     label='Email'
                     name='email'
-                    // value={email} 
-                    // onChange={(e) => setEmail(e.target.value)}
                     value={values.email}
                     onChange={handleInputChange}
                     placeholder='Enter your email'
@@ -134,12 +82,14 @@ function Contacts() {
                   <TextField
                     label='Message'
                     name='message'
-                    // value={message} 
-                    // onChange={(e) => setMessage(e.target.value)}
                     value={values.message}
                     onChange={handleInputChange}
                     multiline minRows={5}
+                    // maxlength = {20}
+                    inputProps={{ maxLength: 260 }}
+                    // inputProps={{min: 0, style: { textAlign: 'center' }}}
                     placeholder='Type your message here...'
+                    helperText={`char... ${values.message.length}/260`}
                     variant='outlined'
                     fullWidth
                     required />
@@ -148,7 +98,6 @@ function Contacts() {
                   <Button type='submit' variant='contained' color='primary' fullWidth>Submit</Button>
                 </Grid>
               </Grid>
-
             </form>
           ) : (
             <Typography variant='h6' align='center'>
